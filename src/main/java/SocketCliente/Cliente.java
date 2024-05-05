47bc7aa9adffbdbe12d41210fc701298df377985
Htr09 <icedohector@gmail.com>
2024-05-04 19:42:10 -0700

    Creación de plantilla para sockets

--- src/main/java/SocketCliente/Cliente.java
+++ src/main/java/SocketCliente/Cliente.java
@@ -8,6 +8,22 @@ package SocketCliente;
  *
  * @author PC
  */
+import java.io.*;
+import java.net.*;
+
 public class Cliente {
-    
+
+    private static final String HOST = "localhost";
+    private static final int PUERTO = 8080;
+
+    public static void main(String[] args) throws Exception {
+        Socket clienteSocket = new Socket(HOST, PUERTO);
+        System.out.println("Conectado al servidor en " + HOST + ":" + PUERTO);
+
+        // Crear flujos de entrada y salida del socket
+        DataInputStream entrada = new DataInputStream(clienteSocket.getInputStream());
+        DataOutputStream salida = new DataOutputStream(clienteSocket.getOutputStream());
+        // Cerrar el socket
+        clienteSocket.close();
+    }
 }
--- src/main/java/SocketServidor/Servidor.java
+++ src/main/java/SocketServidor/Servidor.java
@@ -8,6 +8,51 @@ package SocketServidor;
  *
  * @author PC
  */
+import java.io.*;
+import java.net.*;
+
 public class Servidor {
-    
+
+    private static final int PUERTO = 8080;
+
+    public static void main(String[] args) throws Exception {
+        ServerSocket servidorSocket = new ServerSocket(PUERTO);
+        System.out.println("Servidor escuchando en el puerto " + PUERTO);
+
+        while (true) {
+            Socket clienteSocket = servidorSocket.accept();
+            System.out.println("Cliente conectado desde " + clienteSocket.getInetAddress().getHostAddress());
+
+            // Crear hilos para manejar cada cliente
+            new ManejadorCliente(clienteSocket).start();
+        }
+    }
+
+    private static class ManejadorCliente extends Thread {
+
+        private final Socket clienteSocket;
+
+        public ManejadorCliente(Socket clienteSocket) {
+            this.clienteSocket = clienteSocket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                // Obtener flujos de entrada y salida del socket
+                DataInputStream entrada = new DataInputStream(clienteSocket.getInputStream());
+                DataOutputStream salida = new DataOutputStream(clienteSocket.getOutputStream());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Cliente desconectado");
+            } finally {
+                try {
+                    clienteSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
